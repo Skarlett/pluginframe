8391c38db90eb06ee610c0c80a7ec2b84b5e523c
Skarlett <3759687+Skarlett@users.noreply.github.com>
2019-01-24 19:59:58 -0600

    Official first release
    - Removed some basic myriad features that are easily accessible to developers 
    - Corrected the reload function
    - Fixed Tests

--- tests.py
+++ tests.py
@@ -1,28 +1,48 @@
-import pluginframe
+from pluginframe import HookManager
 from os import mkdir, rmdir, path, remove
-from string import ascii_letters
-from random import choice
 import unittest
+
+
+def write_plugin(fp, data=None):
+    with open(fp, 'w') as f:
+        f.write(data or 'def work(): return 1\ndef setup(handler): \n\thandler.append(work)\n')
 
 
 class PluginFrameTestCase(unittest.TestCase):
     def setUp(self):
-        self.plugin_dir = ''.join(choice(ascii_letters) for _ in range(12))
-        self.plugin = ''
+        self.plugin_dir = '_tests_plugin'
+        self.plugins = (    # Some raw code to generate plugins
+            ('plugin2.py', None), ('plugin1.py', None), ('__init__.py', 'pass'),
+            ('slots_plugin.py', 'def work(): return 1\ndef setup(handler):\n\thandler.append(work)'
+                                '\n__slots__=[\'setup\']')
+        )
 
         mkdir(self.plugin_dir)
-        with open(path.join(self.plugin_dir, self.plugin), 'w') as f:
-            f.write('def work(): return 1;def setup(handler): handler.add(work)')
+        for name, data in self.plugins:
+            write_plugin(path.join(self.plugin_dir, name), data=data)
 
-        self.manager = pluginframe.PluginManager(self.plugin_dir)
+        self.manager = HookManager(self.plugin_dir, excluded_files=[self.plugins[0][0], self.plugins[-1][0]])
         self.handler = list()
 
-    def dynamic_load(self):
+    def test_load(self):
         self.manager.load(self.handler)
         self.assertEqual(len(self.handler), 1)
         self.assertEqual(self.handler[0](), 1)
 
+    def test_reload(self):
+        setattr(self.manager, '_exclusions', set(['slots_plugin.py']))  # Override exclusions
+        self.manager.reload(self.handler)
+        self.assertGreater(len(self.manager.modules), 0)
+
+    def test_slots(self):
+        self.handler = list() # reset this
+        setattr(self.manager, '_exclusions', set(x for x, _ in self.plugins[:-1]))
+        self.manager.reload(self.handler)
+        self.assertEqual(len(self.handler), 1)
+        self.assertEqual(self.handler[0](), 1)
+
     def tearDown(self):
-        remove(path.join(self.plugin_dir, self.plugin))
+        for plug, _ in self.plugins:
+            remove(path.join(self.plugin_dir, plug))
         rmdir(self.plugin_dir)
 
